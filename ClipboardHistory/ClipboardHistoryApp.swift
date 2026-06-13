975a6850cfcc9a2196c2a3e3d3e53e6939313871
Cloncher <andre0001234@gmail.com>
2026-06-13 12:25:21 +0300

    Initial Commit

---
+++ ClipboardHistory/ClipboardHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardHistoryApp.swift
+//  ClipboardHistory
+//
+//  Created by Андрей on 13.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
